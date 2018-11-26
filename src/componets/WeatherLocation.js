3c182c6da5faca0e706da751c42859305b4a2ccb
awce <awce5nk@hotmail.com>
2018-11-25 23:14:34 -0600

    Seccion 3 Clase 10 - Componente Funcional

---
+++ src/componets/WeatherLocation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Componente Funcional con ArrowFuction y codigo JSX
+
+const WeatherLocation = () => (
+    <div>Weather Location</div>
+);
+
+export default WeatherLocation;
