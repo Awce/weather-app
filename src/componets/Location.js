8139a1accf32a04f440810f3fa93a23d174a52db
awce <awce5nk@hotmail.com>
2018-11-26 20:26:35 -0600

    Seccion 3 Clase 16 -Parametro con Object Destructuring

--- src/componets/Location.js
+++ src/componets/Location.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Location = () => (
+// Cuando el AF es una linea se usa () => (), cuando es mas de una linea se usa () => { return(...)}
+const Location = ({ city }) => (
+    
+    // Se pasan las propiedades del un componente externo
+    //const city = props.city;
+
+    // Tecnica por Destructuring de ES6, esto queire decir que cuando tenemos una propiedad con un nombre igual a la variable la asignamos de forma directa
+    //const { city } = props;
+
+    // Otra forma de simplificar el Destructuring es agregarlo al const Location = (props) por const Location = ({ city })
+
     <div>
         <h1>
-            CDMX
+            {city}
         </h1>
     </div>
 );
--- src/componets/WeatherLocation.js
+++ src/componets/WeatherLocation.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 
-// Componente Funcional con ArrowFunction y codigo JSX
+// Componente Funcional con ArrowFunction de ES6 y codigo JSX
 // Un ArrowFunction es una funcion anonima
 
 const WeatherLocation = () => (
 
-    // Incorporamos los componetes
+    // Incorporamos los componetes y enviamos propiedades
     <div>
-        <Location />
+        <Location  city={"CDMX"}/>
         <WeatherData />
     </div>
 );
